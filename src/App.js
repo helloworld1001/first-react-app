8e32fa0f0f4b0ac2180e10724bd1d75613763005
Dzmitry Arhei <demetris167@gmail.com>
2023-04-18 00:06:13 +0300

    createUILibrary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
+  //const [value, setValue] = useState('Текст в инпуте');
+
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript', body: 'Description' },
+    { id: 3, title: 'JavaScript', body: 'Description' },
+    { id: 4, title: 'JavaScript', body: 'Description' },
+    { id: 5, title: 'JavaScript', body: 'Description' },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="Название поста" />
+        <input type="text" placeholder="Описание поста" />
+        <MyButton>Создать пост</MyButton>
+      </form>
+      {/* <ClassCounter /> */}
+      {/* <h1>{value}</h1>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)} /> */}
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   );
 }
